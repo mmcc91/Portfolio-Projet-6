3d27964ad7d2ce5dda8b539bb9bc6ddea049cd5b
mmcc91 <121430505+mmcc91@users.noreply.github.com>
2024-01-17 09:37:41 +0100

    suppresion dans modal

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,14 +5,13 @@ async function showImages() { // fonction pour montrer les images
   const data = await reponse.json();
   works = data
   mesfiltres()
-  displayModificationModal()
+  generateImagesModal(works, "modifier")
 }
 
 //recupertation des pieces depuis fichier
 function generateImages(images, containerId) {
   const gallery = document.getElementById(containerId);
   gallery.innerHTML = ""
-
   //loop creation 
   for (let i = 0; i < images.length; i++) {
     //creations des balises
@@ -91,6 +90,7 @@ const modeEdition = document.querySelector(' header .mode-edition');
 const croiX = document.querySelector(".containerModals .fa-x")
 const modifierModal = document.querySelector(".modifierModal")
 
+
 if (loged != undefined) {  // si loged est diferent de non defini alors on retourne  le mode admin 
   logout.textContent = "logout";
   logout.addEventListener("click", () => {  // au click se deconnect 
@@ -103,56 +103,79 @@ modeEdition.addEventListener("click", () => {
   containerModals.style.display = "flex";
 })
 
-// au click sur la croix se deconnect 
+// au click sur la croix sort de la modal 
 croiX.addEventListener("click", () => {
   containerModals.style.display = "none";
 })
 
-//affiche les images dans la modal
-async function displayModificationModal() {
-  modifierModal.innerHTML = "" // met a vide mon display 
-  const modifier = generateImages(works, "modifier")
-  modifier.forEach(images => {  // on cree les differents ellements quon va avoir besoin  pour afficher 
-    const figure = document.createElement("figure")
-    const img = document.createElement("img")
-    const span = document.createElement("span")
-    const poubelle = document.createElement("i")
-    poubelle.classList.add("fa-solid", "fa-trash-can") // attribut une class a la poubelle
-    poubelle.id = images.id   //va mettre dans la poubelle lelement qui correspond
-    imgElement.src = article.imageUrl;
-    span.appendChild(poubelle) //
-    figure.appendChild(span)
-    figure.appendChild(img)
-    modifierModal.appendChild(figure) // systemen poupee russe 
 
-  });
+
+// je cree ma gallery pour la modal jappel ma fonction en haut de la page 
+function generateImagesModal(images, containerId) {
+  const gallery = document.getElementById(containerId);
+  gallery.innerHTML = ""
+  //loop creation 
+  for (let i = 0; i < images.length; i++) {
+    //creations des balises
+    const article = images[i]; // je cree les articles
+
+    const imageElement = document.createElement("img"); // je cree les images 
+    imageElement.src = article.imageUrl;
+    imageElement.alt = article.title;
+
+    const sectionFigure = document.createElement("figure");
+    sectionFigure.setAttribute("images-id", article.id) // cree un numero d'id
+
+    const figCaptionElement = document.createElement("figCaption"); // je cree figcaption
+    figCaptionElement.innerText = article.title;
+
+    const span = document.createElement("span") //je cree  span
+
+    const poubelle = document.createElement("i") // je cree mon i(icone) et jassocie avec la classe
+    poubelle.classList.add("fa-solid", "fa-trash-can") // attribut une class a la poubelle
+
+    //ratache balises au DOM
+    span.appendChild(poubelle) //
+    span.appendChild(imageElement)
+    sectionFigure.appendChild(span);
+    sectionFigure.appendChild(figCaptionElement);
+    gallery.appendChild(sectionFigure);
+
+  }
+
   supressionImage()
 }
 
-
 //supression   https://www.youtube.com/watch?v=Qkna4uD_qcQ
+//Supression des works grace a la méthode DELETE & au Token user depuis la poubelle de la modale
+//Objet de paramétrage pour requette DELETE avec token
 
-function supressionImage(){
-const poubelleAll = document.querySelectorAll(".fa-trash")
-poubelleAll.forEach(poubelle =>{
-  poubelle.addEventListener("click",(e)=>{
-   let  id = poubelle.id
-   const init ={ // je prepare un object qui sera parametre a mon fetch 
-    method : "DELETE",
-    Headers: {"content-type":"application/json"},
-   }
-   fetch("http://localhost:5678/api/works" + id, init ) // recupere id de chaque poubelle 
-   .then ((response)=>{
-    if (!response.ok){
-      console.log ("delete ne marche pas ")
-    }
-    return reponse.json()
-   })
-   .then((data)=>{
-    console.log ("ca marche voici la data ")
-    displayModificationModal()
-    generateImages()
-  })
-}) 
-});
+
+function supressionImage(images, containerId) {
+  const poubelle = document.querySelectorAll(".fa-trash-can")
+  poubelle.forEach(poubelle => {
+
+    poubelle.addEventListener("click", async (e) => {
+      let sectionFigure = poubelle.parentElement.parentElement
+      let imagesId = sectionFigure.getAttribute("images-id") // recupere un numero d'id
+      const loged = localStorage.getItem("loginResponse");
+      const detail = JSON.parse(loged)
+      await fetch("http://localhost:5678/api/works/" + imagesId, {
+        method: "DELETE", headers: {
+          Authorization: `Bearer ${detail.token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      console.log("delete completed")
+      showImages()
+
+    })
+  }
+
+
+  )
 }
+
+
+
+
